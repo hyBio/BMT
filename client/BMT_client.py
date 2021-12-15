2479a4fe3d6ec26d509628bf524387d49fa30005
hyBio <568038247@qq.com>
2021-12-15 22:17:05 +0800

    使用手册，时间

--- client/BMT_client.py
+++ client/BMT_client.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
-# @Time : 2021/12/13 13:27 
-# @Author : 胡琰 
+# @Time : 2021/12/13 13:27
+# @Author : 胡琰
 # @Version：V 0.1
 # @File : BMT_client.py
 # @Site :
@@ -10,7 +10,7 @@ from ui import main_windows as mw
 from ui import log_in as li
 from ui import register_success as rs
 from ui import register_to as rt
-from PyQt5 import QtCore,QtGui,QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -20,85 +20,130 @@ import sys
 import pandas as pd
 
 
+_translate = QtCore.QCoreApplication.translate
 class main_window(QMainWindow):
-    def __init__(self,parent = None):
+    def __init__(self, parent=None):
         super(main_window, self).__init__(parent)
         self.ui = mw.Ui_BMT_client_main_windows()
         self.ui.setupUi(self)
 
+        # 显示时间
+        timer = QTimer(self)
+        timer.timeout.connect(self.show_current_time)
+        timer.start()
+
+        # 未登录时注销按钮无法触发
         self.ui.log_out.setEnabled(False)
         self.log_in = log_in()
+
+        # 登录跳转
         self.ui.welcome_log_in.clicked.connect(self.close)
         self.ui.welcome_log_in.clicked.connect(self.log_in.show)
 
-        self.register_to=register_to()
+        # 注册跳转
+        self.register_to = register_to()
         self.ui.welcome_register.clicked.connect(self.close)
         self.ui.welcome_register.clicked.connect(self.register_to.show)
         self.ui.log_out.clicked.connect(self.log_out)
 
-        self.time1=QTimer(self)
+        # 设置定时器定时更换书籍图片
+        self.time1 = QTimer(self)
         self.time1.timeout.connect(self.timer_TimeOut)
-        self.time1.start(2000)
-        self.show()
+        self.time1.start(1000)
 
-        self.books_info=pd.read_csv(r"./books_info.csv",header=0)
-        self.n=0
-        self.books=self.books_info.loc[self.n,"books_name"]
-        _translate = QtCore.QCoreApplication.translate
+        # 读取书籍名字进行循环播放
+        self.books_info = pd.read_csv(r"./books_info.csv", header=0)
+        self.n = 0
+        self.books = self.books_info.loc[self.n, "books_name"]
         self.pic = _translate("BMT_client_main_windows", "<html><head/><body><p><img src=\":/books/resource/books_picture/{}.jpg\"/></p></body></html>".format(self.books))
         self.ui.books_recommend_pic.setText(self.pic)
         self.ui.books_on_sale_pic.setText(self.pic)
         self.ui.books_for_you_pic.setText(self.pic)
 
+        # 联系客服
+        self.ui.call_for_help.clicked.connect(self.call_for_help)
+        # 使用手册
+        self.ui.manual.clicked.connect(self.manual)
+
+    def manual(self):
+        QDesktopServices.openUrl(QUrl("https://github.com/hyBio/BMT/blob/master/README.md"))
+
+    def show_current_time(self):
+        datetime = QDateTime.currentDateTime()
+        text = datetime.toString("yyyy年MM月dd日 hh:mm:ss ddd")
+        self.ui.date_time.setText(text)
+        self.ui.date_time.setFont(QFont("Roman times", 12, QFont.Bold))
+
     def timer_TimeOut(self):
-        self.n+=1
-        self.books=self.books_info.loc[self.n,"books_name"]
-        if self.n>99:
-            self.n=0
-        _translate = QtCore.QCoreApplication.translate
+        self.n += 1
+        self.books = self.books_info.loc[self.n, "books_name"]
+        if self.n > 99:
+            self.n = 0
         self.pic = _translate("BMT_client_main_windows", "<html><head/><body><p><img src=\":/books/resource/books_picture/{}.jpg\"/></p></body></html>".format(self.books))
         self.ui.books_recommend_pic.setText(self.pic)
         self.ui.books_on_sale_pic.setText(self.pic)
         self.ui.books_for_you_pic.setText(self.pic)
 
     def log_out(self):
-        _translate = QtCore.QCoreApplication.translate
         self.ui.welcome_log_in.setText(_translate("BMT_client_main_windows", "您好，请登录"))
         self.ui.welcome_log_in.setEnabled(True)
 
+    def call_for_help(self):
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText("huyan35@mail2.sysu.edu.cn")
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "已复制到剪切板"))
+        self.time1 = QTimer(self)
+        self.time1.singleShot(1000,self.timer_TimeOut1)
+
+    def timer_TimeOut1(self):
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "联系客服(点我复制)"))
+
 class log_in(QMainWindow):
-    def __init__(self,parent = None):
+    def __init__(self, parent=None):
         super(log_in, self).__init__(parent)
         self.ui = li.Ui_BMT_client_log_in()
         self.ui.setupUi(self)
-        self.admin_win=AdminWindow()
-        self.database=Database("./account_info.db")
 
+        # 连接管理员账户
+        self.admin_win = AdminWindow()
+        self.database = Database("./account_info.db")
+        # 返回主页
         self.ui.back.clicked.connect(self.back)
+        # 密码显示按钮
         self.ui.checkBox.stateChanged.connect(self.checkBox)
+        # 登录
         self.ui.log_in.clicked.connect(self.log_in)
+        # 忘记密码
         self.ui.password_forget.clicked.connect(self.passwordforget)
+        # 注册
         self.ui.register_to.clicked.connect(self.register_to)
+        # 联系客服
+        self.ui.call_for_help.clicked.connect(self.call_for_help)
 
     def log_in(self):
-        """登录功能实现"""
-        _translate = QtCore.QCoreApplication.translate
+        # 登录功能实现
+        # 获取账户和密码
         username = self.ui.account.text()
         password = self.ui.password.text()
-        data = self.database.find_password_by_username(username)  # 在数据库中查找数据
-        if username and password:  # 如果两个输入框都不为空
+        # 在数据库中查找数据
+        data = self.database.find_password_by_username(username)
+        # 如果两个输入框都不为空
+        if username and password:
             if data:
                 if str(data[0][0]) == password:
                     QMessageBox.information(self, '成功', '欢迎加入BMT:\n{}'.format(username),
                                             QMessageBox.Yes)
-                    self.ui.account.setText('') # 登录成功，将之前的用户信息清除
+                    # 登录成功，将之前的用户信息清除
+                    self.ui.account.setText('')
                     self.ui.password.setText('')
                     self.close()
-                    self.main_window=main_window()
+                    self.main_window = main_window()
                     self.main_window.show()
-                    if username == 'admin':  # 如果是管理员，进入管理界面
+                    # 如果是管理员，进入管理界面
+                    if username == 'admin':
                         self.admin_win.show()
-                    else:  # 如果是普通用户，则进入个人主页，并取消登录按钮的功能
+                    # 如果是普通用户，则进入个人主页，并取消登录按钮的功能
+                    else:
                         self.main_window.ui.welcome_log_in.setText(_translate("BMT_client_main_windows", "{}".format(username)))
                         self.main_window.ui.welcome_log_in.setEnabled(False)
                         self.main_window.ui.log_out.setEnabled(True)
@@ -108,7 +153,8 @@ class log_in(QMainWindow):
                                             QMessageBox.Yes)
             else:
                 QMessageBox.information(self, '错误', '无该账户', QMessageBox.Yes)
-        elif username:  # 如果用户名写了
+        # 如果用户名写了
+        elif username:
             QMessageBox.information(self, '错误', '请输入密码', QMessageBox.Yes)
         else:
             QMessageBox.information(self, '错误', '账户为空', QMessageBox.Yes)
@@ -125,25 +171,50 @@ class log_in(QMainWindow):
             self.ui.password.setEchoMode(QLineEdit.Password)
 
     def passwordforget(self):
-        QMessageBox.information(self, '提示', '请发送邮件联系管理员，单击登录页面\"联系客服\"按钮复制管理员邮箱！', QMessageBox.Yes)
+        QMessageBox.information(self, '提示', '请发送邮件联系管理员，单击登录页面右下角\"联系客服\"按钮复制管理员邮箱！', QMessageBox.Yes)
 
     def register_to(self):
-        self.register_to=register_to()
+        self.register_to = register_to()
         self.close()
         self.register_to.show()
 
+    def call_for_help(self):
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText("huyan35@mail2.sysu.edu.cn")
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "已复制到剪切板"))
+        self.time1 = QTimer(self)
+        self.time1.singleShot(1000,self.timer_TimeOut1)
+
+    def timer_TimeOut1(self):
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "联系客服(点我复制)"))
+
 
 class register_to(QMainWindow):
-    def __init__(self,parent = None):
+    def __init__(self, parent=None):
         super(register_to, self).__init__(parent)
         self.ui = rt.Ui_BMT_client_register_to()
         self.ui.setupUi(self)
         self.database = Database('./account_info.db')
-
+        # 返回主页
         self.ui.back.clicked.connect(self.back)
+        # 已有帐号前往登录
         self.ui.log_in.clicked.connect(self.log_in)
+        # 注册
         self.ui.register_to.clicked.connect(self.register_to)
+        # 显示密码
         self.ui.checkBox.stateChanged.connect(self.checkBox)
+        # 联系客服
+        self.ui.call_for_help.clicked.connect(self.call_for_help)
+
+    def call_for_help(self):
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText("huyan35@mail2.sysu.edu.cn")
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "已复制到剪切板"))
+        self.time1 = QTimer(self)
+        self.time1.singleShot(1000,self.timer_TimeOut1)
+
+    def timer_TimeOut1(self):
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "联系客服(点我复制)"))
 
     def back(self):
         self.main_window = main_window()
@@ -156,20 +227,22 @@ class register_to(QMainWindow):
         self.log_in.show()
 
     def register_to(self):
-        """实现注册功能"""
+        # 实现注册功能
         username = self.ui.account.text()
         password = self.ui.password.text()
         confirm = self.ui.password_2.text()
-
-        if not password or not confirm:  # 如果有一个密码或者密码确认框为空
+        # 如果有一个密码或者密码确认框为空
+        if not password or not confirm:
             QMessageBox.information(self, 'Error', '密码为空',
                                     QMessageBox.Yes)
-        elif self.database.is_has(username):  # 如果用户名已经存在
+        # 如果用户名已经存在
+        elif self.database.is_has(username):
             QMessageBox.information(self, '错误',
                                     '用户已经存在',
                                     QMessageBox.Yes)
         else:
-            if password == confirm and password:  # 如果两次密码一致，并且不为空
+            # 如果两次密码一致，并且不为空
+            if password == confirm and password:
                 if len(username) < 2:
                     QMessageBox.information(self, '错误',
                                             '用户名太短，请输入至少2个字符',
@@ -181,16 +254,18 @@ class register_to(QMainWindow):
                                             QMessageBox.Yes)
                     return
                 else:
-                    self.database.insert_table(username,password)  # 将用户信息写入数据库
+                    # 将用户信息写入数据库
+                    self.database.insert_table(username, password)
                     QMessageBox.information(self, '成功',
                                             '注册成功'.format(username),
                                             QMessageBox.Yes)
-                    self.close()  # 注册完毕之后关闭窗口
+                    # 注册完毕之后关闭窗口
+                    self.close()
             else:
                 QMessageBox.information(self, '错误',
                                         '两次输入的密码不一致，请确认后重新输入',
                                         QMessageBox.Yes)
-            self.register_success=register_success()
+            self.register_success = register_success()
             self.register_success.show()
 
     def checkBox(self):
@@ -201,22 +276,36 @@ class register_to(QMainWindow):
 
 
 class register_success(QMainWindow):
-    def __init__(self,parent = None):
+    def __init__(self, parent=None):
         super(register_success, self).__init__(parent)
         self.ui = rs.Ui_BMT_client_register()
         self.ui.setupUi(self)
-
+        # 返回主页
         self.ui.back.clicked.connect(self.back)
+        # 登录
         self.ui.log_in.clicked.connect(self.log_in)
+        # 退出
         self.ui.exit.clicked.connect(self.close)
+        # 联系客服
+        self.ui.call_for_help.clicked.connect(self.call_for_help)
+
+    def call_for_help(self):
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText("huyan35@mail2.sysu.edu.cn")
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "已复制到剪切板"))
+        self.time1 = QTimer(self)
+        self.time1.singleShot(1000,self.timer_TimeOut1)
+
+    def timer_TimeOut1(self):
+        self.ui.call_for_help.setText(_translate("BMT_client_main_windows", "联系客服(点我复制)"))
 
     def back(self):
-        self.main_window=main_window()
+        self.main_window = main_window()
         self.close()
         self.main_window.show()
 
     def log_in(self):
-        self.log_in=log_in()
+        self.log_in = log_in()
         self.close()
         self.log_in.show()
 
